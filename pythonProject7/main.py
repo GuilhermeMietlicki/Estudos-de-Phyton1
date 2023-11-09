9b1ee9b6663bff7e0e944de0b7764d533714dc08
GuilhermeMietlicki <gui.mietlicki@gmail.com>
2023-11-09 15:55:57 -0300

    first commit

--- pythonProject7/main.py
+++ pythonProject7/main.py
@@ -1,4 +1,4 @@
 n1 = float(input('Primeira nota do aluno: '))
 n2 = float(input('Segunda nota do aluno: '))
 média = (n1 + n2) / 2
-print('A media entrte {} e {} é igual a {}'.format (n1, n2, média))
+print('A media entrte {:.1f} e {:.1f} é igual a {:.1f}'.format (n1, n2, média))
