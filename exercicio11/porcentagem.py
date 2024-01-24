3858eea1aaf4c9b9cb944891e0d7cadbc49843b1
GuilhermeMietlicki <gui.mietlicki@gmail.com>
2024-01-24 13:26:01 -0300

    exercicio12: Calculo de porcentagem.

---
+++ exercicio11/porcentagem.py
@@ -0,0 +1,5 @@
+preço = float(input('Qual é o preço do produto? R$'))
+novo = preço - (preço * 5 / 100)
+
+# preço -5%
+print('O produto que custava, {:.2f}, na promoção com desconto de 5% vai custar, {:.2f}.'.format(preço, novo))
